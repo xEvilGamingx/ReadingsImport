05cf5a7010a2275410c62d547d7a166fef85c693
julian Tschersich <julian.tschersich@t-online.de>
2023-10-02 00:17:07 +0200

    init repository

---
+++ Models/Step.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ReadingsImport.Models
+{
+    public class Step
+    {
+        public string Name { get; set; }
+        public RenderFragment Component { get; set; }
+        public bool Selected { get; set; }
+
+        public Step Previous { get; set; }
+        public Step Next { get; set; }
+    }
+}
